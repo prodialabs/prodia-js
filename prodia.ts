da191168878a156ca07d84232c40c0017620b26c
Monty Anderson <work@montyanderson.net>
2023-08-02 19:28:10 +0100

    `*`: 🚀

---
+++ prodia.ts
@@ -0,0 +1,142 @@
+/* Job Responses */
+
+type ProdiaJobBase = { job: string };
+
+export type ProdiaJobQueued = ProdiaJobBase & { status: "queued" };
+export type ProdiaJobGenerating = ProdiaJobBase & { status: "generating" };
+export type ProdiaJobFailed = ProdiaJobBase & { status: "failed" };
+export type ProdiaJobSucceeded = ProdiaJobBase & {
+	status: "succeeded";
+	imageUrl: string;
+};
+
+export type ProdiaJob =
+	| ProdiaJobQueued
+	| ProdiaJobGenerating
+	| ProdiaJobFailed
+	| ProdiaJobSucceeded;
+
+/* Generation Requests */
+
+export type ProdiaGenerateRequest = {
+	prompt: string;
+	model?: string;
+	negative_prompt?: string;
+	steps?: number;
+	cfg_scale?: number;
+	seed?: number;
+	upscale?: boolean;
+	sampler?: string;
+	aspect_ratio?: "square" | "portrait" | "landscape";
+};
+
+export type ProdiaTransformRequest = {
+	imageUrl: string;
+	prompt: string;
+	model?: string;
+	denoising_strength?: number;
+	negative_prompt?: string;
+	steps?: number;
+	cfg_scale?: number;
+	seed?: number;
+	upscale?: boolean;
+	sampler?: string;
+};
+
+/* Constructor Definions */
+
+export type Prodia = ReturnType<typeof createProdia>;
+
+export type CreateProdiaOptions = {
+	apiKey: string;
+	base?: string;
+};
+
+export const createProdia = ({ apiKey, base: _base }: CreateProdiaOptions) => {
+	const base = _base || "https://api.prodia.com/v1";
+
+	const headers = {
+		"X-Prodia-Key": apiKey
+	};
+
+	const generate = async (params: ProdiaGenerateRequest) => {
+		const response = await fetch(`${base}/job`, {
+			method: "POST",
+			headers: {
+				...headers,
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(params)
+		});
+
+		if (response.status !== 200) {
+			throw new Error(`Bad Prodia Response: ${response.status}`);
+		}
+
+		return (await response.json()) as ProdiaJobQueued;
+	};
+
+	const transform = async (params: ProdiaTransformRequest) => {
+		const response = await fetch(`${base}/transform`, {
+			method: "POST",
+			headers: {
+				...headers,
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(params)
+		});
+
+		if (response.status !== 200) {
+			throw new Error(`Bad Prodia Response: ${response.status}`);
+		}
+
+		return (await response.json()) as ProdiaJobQueued;
+	};
+
+	const getJob = async (jobId: string) => {
+		const response = await fetch(`${base}/job/${jobId}`, {
+			headers
+		});
+
+		if (response.status !== 200) {
+			throw new Error(`Bad Prodia Response: ${response.status}`);
+		}
+
+		return (await response.json()) as ProdiaJob;
+	};
+
+	const wait = async (job: ProdiaJobQueued | ProdiaJobGenerating) => {
+		let jobResult: ProdiaJob = job;
+
+		while (
+			jobResult.status !== "succeeded" &&
+			jobResult.status !== "failed"
+		) {
+			await new Promise((resolve) => setTimeout(resolve, 250));
+
+			jobResult = await getJob(job.job);
+		}
+
+		return jobResult as ProdiaJobSucceeded | ProdiaJobFailed;
+	};
+
+	const listModels = async () => {
+		const response = await fetch(`${base}/models/list`, {
+			headers
+		});
+
+		if (response.status !== 200) {
+			throw new Error(`Bad Prodia Response: ${response.status}`);
+		}
+
+		return (await response.json()) as string[];
+	};
+
+	return {
+		generate,
+		transform,
+		wait,
+		getJob,
+		listModels
+	};
+};
---
+++ readme.md
@@ -0,0 +1,67 @@
+# [![Prodia](./logo.svg)](https://prodia.com)
+
+[![npm version](https://badge.fury.io/js/prodia.svg)](https://badge.fury.io/js/prodia)
+
+Official TypeScript library for Prodia's AI inference API.
+
+-   [Get an API Key](https://app.prodia.com/api)
+
+-   [View Docs + Pricing](https://docs.prodia.com/reference/getting-started)
+
+## Usage
+
+```
+npm install prodia --save
+```
+
+```javascript
+import { createProdia } from "prodia";
+
+const prodia = createProdia({
+	apiKey: "..."
+});
+
+(async () => {
+	const job = await prodia.generate({
+		prompt: "puppies in a cloud, 4k"
+	});
+
+	const { imageUrl, status } = await prodia.wait(job);
+
+	// check status and view your image :)
+})();
+```
+
+## API
+
+### `createProdia(opts)` => `Prodia`
+
+Create a new Prodia object.
+
+#### `opts`
+
+-   `apiKey` (`string`)
+
+Your API key from the Prodia dashboard.
+
+-   `base` (`string`)
+
+Optional: an enterprise-specific Prodia island.
+
+#### `Prodia`
+
+##### `generate(` `params` `)` => `ProdiaJob`
+
+Create a new text to image job.
+
+Supports all parameters [listed in the documentation](https://docs.prodia.com/reference/generate).
+
+##### `transform(` `params` `)` => `ProdiaJob`
+
+Create a new image to image job.
+
+Supports all parameters [listed in the documentation](https://docs.prodia.com/reference/transform).
+
+##### `wait(` `ProdiaJob` `)` => `{` `status`, `imageUrl` `}`
+
+Returns once as job has completed.
