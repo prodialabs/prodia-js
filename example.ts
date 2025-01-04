c7c848cde4005d40aab7b49a87389eaa8997883f
Monty Anderson <work@montyanderson.net>
2025-01-04 21:29:21 +0000

    `*`: add multi-version node tests

---
+++ example.ts
@@ -0,0 +1,31 @@
+import { createProdia } from "./v2";
+
+const token = Deno.env.get("PRODIA_TOKEN");
+
+if (typeof token !== "string") {
+	throw new Error("PRODIA_TOKEN is not set");
+}
+
+const isJpeg = (image: ArrayBuffer): boolean => {
+	const view = new Uint8Array(image);
+
+	return view[0] === 0xff && view[1] === 0xd8;
+};
+
+const client = createProdia({
+	token,
+});
+
+const job = await client.job({
+	type: "inference.flux.dev.txt2img.v1",
+	config: {
+		prompt: "puppies in a cloud, 4k",
+		steps: 1,
+		width: 1024,
+		height: 1024,
+	},
+});
+
+const image = await job.arrayBuffer();
+
+assertEquals(isJpeg(image), true, "Image should be a JPEG");
